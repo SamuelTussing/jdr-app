cd80241335c0fab53a3ef97b953c25f7df9bd8b8
Tussing Samuel <sml.tussing@gmail.com>
2025-09-18 10:43:27 +0200

    mise a jour

--- src/app/api/auth/login/route.js
+++ src/app/api/auth/login/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server"
 import clientPromise from "@/lib/mongodb"
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs"
 
 export async function POST(req) {
   try {

f6bb2a70f3d359628721cf91b7179e4f9398286a
Tussing Samuel <sml.tussing@gmail.com>
2025-09-22 10:01:14 +0200

    probleme logs vercel

--- src/app/accueil/page.js
+++ src/app/accueil/page.js
@@ -1,16 +1,19 @@
-import TopBar from "@/components/TopBar"
-import Header from "@/components/Header"
-import HeroSection from "@/components/HeroSection"
-import ProductCarousel from "@/components/ProductCarousel"
-import FeaturedGames from "@/components/FeaturedGames"
+import TopBar from "./components/TopBar"
+import Header from "./components/Header"
+import HeroSection from "./components/HeroSection"
+import ProductCarousel from "./components/ProductCarousel"
+import FeaturedGames from "./components/FeaturedGames"
 import "./accueil.css"
 
 export default function AccueilPage() {
 
-  const handleDisconnectClick = () => {
-    sessionStorage.removeItem("user")
-    router.replace("/login")
-  }
+    useEffect(() => {
+    const storedUser = sessionStorage.getItem("user")
+    if (storedUser) {
+      const user = JSON.parse(storedUser)
+      if (user.username) setPseudo(user.username)
+    }
+  }, [])
 
 
   return (

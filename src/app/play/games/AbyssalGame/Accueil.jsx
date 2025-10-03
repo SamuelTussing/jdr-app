b60720dc2f105518b9653ce956682ea61db48ffa
Tussing Samuel <sml.tussing@gmail.com>
2025-10-03 09:08:06 +0200

    maj jeu

--- src/app/play/games/AbyssalGame/Accueil.jsx
+++ src/app/play/games/AbyssalGame/Accueil.jsx
@@ -1,34 +1,25 @@
-"use client"
-import Image from "next/image";
-import { useState } from "react"
+'use client'
 import "./accueilaustralabyss.css"
 import Link from "next/link"
 
-/* PAGE ACCUEIL DU JEU*/
-
-
-export default function Accueil({ onNext }) {
-
-
-
+export default function Accueil({ goTo }) {
   return (
     <div className="game-container1">
       <div className="game-background">
         <div className="game-content">
           <h1 className="game-title">Nouvelle Partie</h1>
 
-            <div className="button-container">
-              <button className="game-button" onClick={onNext} >
-                Commencer aventure
-              </button>
-              <button className="game-button inactif" >
-                Continuer
-              </button>
-              <Link href="/accueil" className="game-button">
-                Retour au menu
-              </Link>
-            </div>
+          <div className="button-container">
+            <button className="game-button" onClick={() => goTo("choix")}>
+              Commencer aventure
+            </button>
+            <button className="game-button inactif">Continuer</button>
+            <Link href="/accueil" className="game-button">
+              Retour au menu
+            </Link>
           </div>
         </div>
       </div>
-  )}
+    </div>
+  )
+}

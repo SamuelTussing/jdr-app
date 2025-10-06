f540234caf50fa97bd7b4f00b0ba3e0ffc3165ad
Tussing Samuel <sml.tussing@gmail.com>
2025-10-06 16:02:29 +0200

    la page jeu n'est pas accessible, tentative de correction 404 page introuvable

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    appDir: true, // active la prise en charge du dossier app
+  },
+  // permet à Next.js de reconnaître les routes API dans src/app
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+}
 
-export default nextConfig;
+export default nextConfig
